0cc3578b1bd4ac5593b96cde6a7700a1ef69ecd8
Kaloyan Raev <kaloyan@raev.name>
2015-03-12 23:24:09 +0200

    Update site for licensed feature

--- org.eclipse.licensing/src/org/eclipse/licensing/LicensingUtils.java
+++ org.eclipse.licensing/src/org/eclipse/licensing/LicensingUtils.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.commons.lang.SystemUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 public class LicensingUtils {
 	
