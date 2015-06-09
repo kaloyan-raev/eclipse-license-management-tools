cd5e5f9e10796f9505ee9dd18ec893318a576a5c
Kaloyan Raev <kaloyan@raev.name>
2015-06-09 10:04:44 +0300

    Copyright notice in source code

--- framework/org.eclipse.licensing.core/src/org/eclipse/licensing/core/ILicensedProduct.java
+++ framework/org.eclipse.licensing.core/src/org/eclipse/licensing/core/ILicensedProduct.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Kaloyan Raev.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kaloyan Raev - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.licensing.core;
 
 import java.security.PublicKey;
